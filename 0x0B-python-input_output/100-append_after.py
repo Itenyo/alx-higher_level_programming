7f32db1b45799f62597c63002cbf50504117384e
Itenyo <kevinmmasi3@gmail.com>
2022-07-06 06:17:56 -0700

    git

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""append after module"""
+"""A function that inserts a line of text"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """append a text after a substring line"""
-
+    """A function that inserts a line of text"""
     with open(filename, 'r') as f:
-        content = f.readlines()
-        for (index, line) in enumerate(content):
-            if line.find(search_string) != -1:
-                content.insert(index+1, new_string)
-        new_content = "".join(content)
-    f = open(filename, 'w')
-    f.write(new_content)
-    f.close()
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
+            if search_string in line:
+                f.write(line + new_string)
+            else:
+                f.write(line)
